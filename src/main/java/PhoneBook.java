b22b535e44223e0eef2cd664bc645362c298fcbe
akhramova <xaa2128@mail.ru>
2024-02-17 13:58:52 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private static Map<String, String> book;
+
+    public PhoneBook() {
+        book = new HashMap<>();
+    }
+
     public int add(String name, String phoneNumber) {
-        return 0;
+        book.put(phoneNumber, name);
+        return book.size();
     }
 }
