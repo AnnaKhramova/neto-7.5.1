865a74d6554d1678d4b64ac57bec6028dd68dd99
akhramova <xaa2128@mail.ru>
2024-02-17 13:55:16 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    @Test
+    public void addTest() {
+        PhoneBook book = new PhoneBook();
+        int result = book.add("Виталик", "+9 999 555 36 36");
+        Assertions.assertEquals(result, 1);
+    }
 }
